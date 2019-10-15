84ea8924e1485a71eee129bde40cdac1cfcbee8d
Abu Naser Mohammad Hafizur Rahman <anmhafiz@gmail.com>
2019-10-15 14:32:52 +0600

    Bank Account

---
+++ AccountOperationAppPractice1/AccountOperationAppPractice1/AccountOperationUi.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AccountOperationAppPractice1
+{
+    public partial class AccountOperationUi : Form
+    {
+        public AccountOperationUi()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        BankAccount bankAccount = new BankAccount();
+
+        private void CreateButton_Click(object sender, EventArgs e)
+        {
+            
+
+            bankAccount.customerName = customerNameTextBox.Text;
+            bankAccount.accountNumber = accountNoTextBox.Text;
+        }
+
+        private void DipositButton_Click(object sender, EventArgs e)
+        {
+            
+
+            double amount = Convert.ToDouble(amountTextBox.Text);
+            bool isDeposited = bankAccount.Deposit(amount);
+            if (isDeposited)
+            {
+                amountTextBox.Text = " ";
+            }
+        }
+
+        private void WithdrawButton_Click(object sender, EventArgs e)
+        {
+            
+
+
+            double amount = Convert.ToDouble(amountTextBox.Text);
+            bool isWithdrawn = bankAccount.Withdraw(amount);
+            if (isWithdrawn)
+            {
+                amountTextBox.Text = " ";
+            }
+        }
+
+        private void ReportButton_Click(object sender, EventArgs e)
+        {
+            
+
+            string message = bankAccount.GetInfo();
+            MessageBox.Show(message);
+        }
+    }
+}
---
+++ AccountOperationAppPractice1/AccountOperationAppPractice1/BankAccount.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountOperationAppPractice1
+{
+    class BankAccount
+    {
+        public string accountNumber;
+        public string customerName;
+        private double balance;
+        public bool Deposit(double amount)
+        {
+            balance = balance + amount;
+            return true;
+        }
+
+        public bool Withdraw(double amount)
+        {
+            balance = balance - amount;
+
+            return true;
+        }
+
+        public string GetInfo()
+        {
+            var message = customerName + ", your account number: " + accountNumber + " and its balance: " + balance;
+            return message;
+        }
+    }
+}
